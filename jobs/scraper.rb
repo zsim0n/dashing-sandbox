b483334f2a8111bdb452e916572833d1785eb1a4
zsim0n <zoltan@zoltansimon.me>
2013-12-13 22:39:30 +0100

    cleanup scraper.rb

--- jobs/scraper.rb
+++ jobs/scraper.rb
@@ -2,13 +2,10 @@
 require 'open-uri'
 require 'nokogiri'
 
-# uri = 'http://www.kvb-koeln.de/generated/?aktion=show&code=46'
 uri = 'http://trafikinfo.bane.dk/TrafikInformation/AfgangAnkomst/Ankomst/VAL/Fjerntog/UdvidetVisning'
 
 SCHEDULER.every '30s', :first_in => 0 do |job|
 
-# windows encoding. Nice one, KVB!
-#  source   = open(uri, 'r:windows-1252').read()
   source   = open(uri).read()
   dom      = Nokogiri::HTML.parse(source)
   table    = dom.css('table#ankomsttabel')
